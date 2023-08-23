ae352f27dcd691fb68a18c6de50145583999b25e
ElpisHelle <elpisHelle0710@gmail.com>
2023-08-23 15:18:33 -0700

    updated

--- src/components/Layout/HeaderPage.vue
+++ src/components/Layout/HeaderPage.vue
@@ -164,7 +164,7 @@
                     FINANCE ACCOUNTING 
                   </router-link>
                 </li>
-                <div class="search_control mt-2 ">
+                <li class="search_control mt-2 ">
                   <input
                       type="text"
                       placeholder="Search.."
@@ -184,10 +184,10 @@
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
                       </svg>
                     </button>
-                </div>
+                  </li>
               </ul>
         </div>
-        <div class="nav_button d-flex justify-content-center align-items-center" >
+        <div class="nav_button " >
           <div @click="handleChange" :class="{change: changeclass}">
             <div class="bar1"></div>
             <div class="bar2"></div>
